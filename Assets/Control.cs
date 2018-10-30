a121df53d8c1cdedac5292b107d5be68d8043827
Mengyu CHen <mengyuchen@ucsb.edu>
2018-10-29 17:33:31 -0700

    first commit

---
+++ Assets/Control.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+	
+	public float number{
+		get{
+			return _number * 10;
+		}
+		set{
+			_number = value * 2;
+		}
+	}	
+	private float _number = 0.1f;
+}
---
+++ Assets/accessor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class accessor : MonoBehaviour {
+
+	Control control;
+	void Start () {
+		//find control object
+		control = FindObjectOfType<Control>();
+		control.number = 10.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
